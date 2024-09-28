8e3e14859394ec3c5270465b3061da54e9b0559e
TRIVlAL <arnaud.loche@gmail.com>
2024-09-28 17:10:57 +0200

    Removed debugging code

--- src/entities/spinner.h
+++ src/entities/spinner.h
@@ -17,13 +17,6 @@ namespace gigno {
     private:
         virtual void Think(float dt) override {
             RenderedEntity::Think(dt);
-
-            if(Transform.rotation.y + (Speed * dt) > glm::two_pi<float>()) {
-                std::cout << "ROTATION AT TIME " << std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()) << std::endl;
-                std::cout << "TIME SINCE LAST ROTATION" << (std::chrono::system_clock::now() - last_rotation).count() << std::endl;
-                last_rotation = std::chrono::system_clock::now();
-            }
-
             Transform.rotation.y = glm::mod<float>(Transform.rotation.y + (Speed * dt), glm::two_pi<float>());
 
         }
