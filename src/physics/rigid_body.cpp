6580c91a61ed9734a253ff35973776202588dc39
TRIVlAL <arnaud.loche@gmail.com>
2024-10-31 18:55:57 +0100

    Unfinished Rigidbody prototype

---
+++ src/physics/rigid_body.cpp
@@ -0,0 +1,29 @@
+#include "rigid_body.h"
+
+namespace gigno {
+
+    RigidBody::RigidBody(ModelData_t model)
+    : RenderedEntity(model) {
+
+    }
+
+    RigidBody::~RigidBody() {
+
+    }
+
+    void RigidBody::Think(float dt) {
+        if(TestingInterpolateType == 0) {
+            m_Velocity += dt * m_Force;
+            Transform.Position += dt * m_Velocity;
+        } else if(TestingInterpolateType == 1) {
+            glm::vec3 avrg_vel = m_Velocity;
+            m_Velocity += dt * m_Force;
+            avrg_vel += m_Velocity;
+            avrg_vel *= 0.5f;
+            Transform.Position += dt * avrg_vel;
+        }
+
+        m_Force = glm::vec3{0.0f};
+    }
+
+}
