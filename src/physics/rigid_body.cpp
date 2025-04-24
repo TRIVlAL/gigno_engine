a0672f7f66bf475cd9ce1f04b2c8c2d809abb832
TRIVlAL <arnaud.loche@gmail.com>
2025-04-24 23:07:02 +0200

    fixed 'bouncing' bug

--- src/physics/rigid_body.cpp
+++ src/physics/rigid_body.cpp
@@ -74,56 +74,64 @@ namespace gigno {
     }
 
     void RigidBody::LatePhysicThink(float dt) {
+        
+        if(ColliderType == COLLIDER_HULL) {
+            UpdateTransformedModel();
+        }
+        UpdateCollider();
+        UpdateInertiaTensor(); //?
+
+        if(IsStatic) {
+            return;
+        }
+
+        //Gravity
+        if(!m_GravityDisabled) {
+            AddForce((glm::vec3)convar_phys_gravity * Mass);
+        }
+        m_GravityDisabled = false;
+
+        //Drag
+        // Uses a linear approximation as can be seen in Unity or Godot.
+        float ldrag = glm::clamp(1.0f - (Drag * dt), 0.0f, 1.0f);
+        Velocity *= ldrag;
+        float rdrag = glm::clamp(1.0f - (AngularDrag * dt), 0.0f, 1.0f);
+        AngularVelocity *= rdrag;
+
+        const float epsilon = 0.00000000001;
+        if(LenSquared(Velocity) < epsilon) {
+            Velocity  = glm::vec3{0.0f};
+        }
+        if(LenSquared(AngularVelocity) < epsilon) {
+            AngularVelocity = glm::vec3{0.0f};
+        }
+
+        glm::vec3 avrg_vel = Velocity;
+        Velocity += dt * Force / Mass;
+        avrg_vel += Velocity;
+        avrg_vel *= 0.5f;
+        glm::vec3 applied_vel = dt * avrg_vel;
+        Position += applied_vel;
+
+        glm::vec3 avrg_rot_vel = AngularVelocity;
+        AngularVelocity += dt * (Torque / Mass) * GetInverseInertiaTensor();
+        avrg_rot_vel += AngularVelocity;
+        avrg_rot_vel *= 0.5f;
+
+        if(!LockRotation) {
+            Rotation += glm::quat{0.0f, avrg_rot_vel * dt * 0.5f} * Rotation;
+            Rotation = glm::normalize(Rotation);
+        }
+
+        Force = glm::vec3{0.0f};
+        Torque = glm::vec3{0.0f};
+
+        
         if(ColliderType == COLLIDER_HULL) {
             UpdateTransformedModel();
         }
         UpdateCollider();
         UpdateInertiaTensor();
-
-        if(!IsStatic) {
-
-            //Gravity
-            if(!m_GravityDisabled) {
-                AddForce((glm::vec3)convar_phys_gravity * Mass);
-            }
-            m_GravityDisabled = false;
-
-            //Drag
-            // Uses a linear approximation as can be seen in Unity or Godot.
-            float ldrag = glm::clamp(1.0f - (Drag * dt), 0.0f, 1.0f);
-            Velocity *= ldrag;
-            float rdrag = glm::clamp(1.0f - (AngularDrag * dt), 0.0f, 1.0f);
-            AngularVelocity *= rdrag;
-
-            const float epsilon = 0.00000000001;
-            if(LenSquared(Velocity) < epsilon) {
-                Velocity  = glm::vec3{0.0f};
-            }
-            if(LenSquared(AngularVelocity) < epsilon) {
-                AngularVelocity = glm::vec3{0.0f};
-            }
-
-            glm::vec3 avrg_vel = Velocity;
-            Velocity += dt * Force / Mass;
-            avrg_vel += Velocity;
-            avrg_vel *= 0.5f;
-            glm::vec3 applied_vel = dt * avrg_vel;
-            Position += applied_vel;
-
-            glm::vec3 avrg_rot_vel = AngularVelocity;
-            AngularVelocity += dt * (Torque / Mass) * GetInverseInertiaTensor();
-            avrg_rot_vel += AngularVelocity;
-            avrg_rot_vel *= 0.5f;
-
-            if(!LockRotation) {
-                Rotation += glm::quat{0.0f, avrg_rot_vel * dt * 0.5f} * Rotation;
-                Rotation = glm::normalize(Rotation);
-            }
-
-            Force = glm::vec3{0.0f};
-            Torque = glm::vec3{0.0f};
-
-        }
     }
 
     void RigidBody::CleanUp() {
